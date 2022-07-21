b25e3e82cec13c1d636d86d8316c5002a9bef787
DanielCArce <daniel.camposarce@gmail.com>
2022-07-21 07:25:47 -0600

    UPDATE SMALL STRUCTURES

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,41 @@
 import Head from "next/head";
-import FormComponent from "./../components/FormComponent";
-import ResumeComponent from "./../components/ResumeComponent";
-import { useReducer } from "react";
+import React, { useReducer } from "react";
+import FormComponent from "../components/FormComponent";
 import { CalculateContext, initialState } from "../contexts/CalculateContext";
 import ReducerFunction from "../reducer/ReducerFunction";
+import DisplayInfoComponent from "./../components/DisplayInfoComponent";
 export default function Home() {
   const [state, dispatch] = useReducer(ReducerFunction, initialState);
   return (
-    <>
+    <React.Fragment>
       <Head>
         <title>Calculo de Porcentaje Latam</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <CalculateContext.Provider value={{ state, dispatch }}>
-        <div className="flex flex-row gap-7 px-10 py-4 justify-center">
-          <FormComponent />
-          <ResumeComponent />
+        <div className="flex flex-row justify-center">
+          <div>
+            <h1>Calculadora de Bigo</h1>
+            <h2>Balance de % Exterior</h2>
+            <h3>Aclaración del calculador</h3>
+            <ul>
+              <li>
+                Este calculador no esta asoaciado de ninguna manera con Bigo
+                Live ni ninguna empresa asociada.
+              </li>
+              <li>
+                Los calculos se realizan de acuerdo a la información
+                suministrada por el usuario, por lo tanto solo se es responsable
+                por cualquier dato errado de margen de error.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <FormComponent />
+            <DisplayInfoComponent />
+          </div>
         </div>
       </CalculateContext.Provider>
-    </>
+    </React.Fragment>
   );
 }
