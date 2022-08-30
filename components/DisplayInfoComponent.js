6821eeae7ffff3b4f7955cdfcb64bb3abc3b45e7
DanielCArce <daniel.camposarce@gmail.com>
2022-08-30 09:24:20 -0600

    Add Share button

--- components/DisplayInfoComponent.js
+++ components/DisplayInfoComponent.js
@@ -3,6 +3,16 @@ import { CalculateContext } from "../contexts/CalculateContext";
 
 function DisplayInfoComponent() {
   const { state } = useContext(CalculateContext);
+  const handleShare = () => {
+    let sharedData = {
+      title: "Calculo Para Compensar Porcentage",
+      url: "https://bigo-calculator.vercel.app",
+      text: `Los diamantes para compensar es un total de ${Intl.NumberFormat(
+        "en-US"
+      ).format(state.seeds_for_balance)}`,
+    };
+    navigator.share(sharedData);
+  };
   return (
     <div>
       <table className="border-separate">
@@ -45,6 +55,13 @@ function DisplayInfoComponent() {
             <td>{Intl.NumberFormat("en-US").format(state.seeds_total)}</td>
           </tr>
         </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="2" className="text-center border-b-2">
+              <button onClick={handleShare}>Compartir Calculo </button>
+            </td>
+          </tr>
+        </tfoot>
       </table>
     </div>
   );
