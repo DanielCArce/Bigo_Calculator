5bbe2417a83ad97a6372d0f2aee6aa791afcc343
DanielCArce <daniel.camposarce@gmail.com>
2022-09-12 13:20:25 -0600

    Remove GTM

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head/>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NQ2SNN9"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript>
         <Main />
         <NextScript />
       </body>
