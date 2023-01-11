dcda6e3826e78b04d5c7828136c42d7f35820e03
DanielCArce <daniel.camposarce@gmail.com>
2023-01-11 09:13:02 -0600

    Add margin to footer

--- components/layout/FooterPage.jsx
+++ components/layout/FooterPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function FooterPage() {
   return (
-      <footer>
+      <footer className="mt-8">
           <span>
               Recorda que si tienes alguna sugerencia para mejorar, para implementar una nueva herramienta puede manndarme un mensaje a la cuenta de bigo de <Link href="https://slink.bigovideo.tv/KWIO3G">
               <a>Sr. Aguacate @ Bigo</a></Link> o por Telegram <Link href="https://t.me/SrAvocado"><a>Sr. Aguacate @ Telegram</a></Link>
