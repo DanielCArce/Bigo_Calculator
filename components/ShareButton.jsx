953893f818c57c29811d4bdca1ac531fc4b85286
DanielCArce <daniel.camposarce@gmail.com>
2024-01-30 20:12:42 -0600

    rollback share button

--- components/ShareButton.jsx
+++ components/ShareButton.jsx
@@ -3,26 +3,16 @@ import {toBlob} from 'html-to-image'
 function ShareButton({ data, img }) {
     //const route = useRouter()
     const handleShare = () => {
-        const tempfile = toBlob(img.current).then((blob) => new File([blob], 'estimations.png', { type: 'image/png' }))
-        navigator.share({
-                        files: [tempFile],
-                        text: `Detalles del calculo de ${data.type}`,
-                        title: `Detalle Calculo`,
-                        url:'https://bigo-calculator.vercel.app'
-                    })
-        // Notification.requestPermission().then((permission) => {
-        //     if (permission == "granted") {
-        //         const noty = new Notification('Listo para compartir', { body: 'Imagen Generada exitosamente' })
-        //         noty.addEventListener('close', (e) => {
-        //             navigator.share({
-        //                 files: [tempFile],
-        //                 text: `Detalles del calculo de ${data.type}`,
-        //                 title: `Detalle Calculo`,
-        //                 url:'https://bigo-calculator.vercel.app'
-        //             })
-        //         })
-        //     }
-        // })
+        toBlob(img.current).then((blob) => {
+            const file = new File([blob], 'estimation.png', { type: 'image/png' });
+            navigator.share({
+            title: data.title,
+            url: `https://bigo-calculator.vercel.app`,
+            text: `Para Compensar un ${data.percentage} de ${data.type} se requiere de ${data.balance_seeds} diamantes.`,
+            text: `Para Compensar un ${data.percentage} de ${data.type} se requiere de ${Math.ceil(data.balance_seeds)} diamantes.`,
+            files: [file]
+            })
+        })
     }
     return (<button className="bg-red-500 text-slate-100 py-2 px-4" onClick={handleShare}>Compartir Resultado</button>)
 }
